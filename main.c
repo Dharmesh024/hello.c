faad503c9a157f0d0e6decc8ed2e219fe78acb9e
Dharmesh024 <144957188+Dharmesh024@users.noreply.github.com>
2024-07-27 01:43:29 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    // int length = 3;
-    // int breadth = 6;
-    int length, breadth;
-    printf("Enter length\n");
-    scanf("%d", &length);
-
-    printf("Enter breadth\n");
-    scanf("%d", &breadth);
-
-    printf("The area of this rectangle is %d", length*breadth);
+int main()
+{
+    printf("Hello world!\n");
     return 0;
 }
